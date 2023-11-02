fee9691be694962f8370bfe5afa386d64f5e7c31
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2023-11-02 21:32:58 +0300

    Create Maximum average subarray I.py

--- Maximum average subarray I.py
+++ Maximum average subarray I.py
@@ -1,1 +1,17 @@
 
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        current = 0
+        for i in range(k):
+            current += nums[i]
+            max_sum = current
+        for i in range(k, len(nums)):
+            current += (nums[i] - nums[i - k])
+            max_sum = max(max_sum, current)
+
+        return max_sum/float(k)
