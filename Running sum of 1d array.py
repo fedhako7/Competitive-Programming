4f76797ae8e83fb1bbfd66c0ae2f0a11fbdbcd38
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2023-11-08 20:35:23 +0300

    Create Running sum of 1d array.py

---
+++ Running sum of 1d array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def runningSum(self, nums):
+        sum = 0
+        for i in range(len(nums)):
+            sum += nums[i]
+            nums[i] = sum
+
+        return nums
