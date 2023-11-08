0a737b912a2bc7b0d72b2c6ef35603121826c171
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2023-11-08 16:46:03 +0300

    Create Minimum window substring.py

---
+++ Minimum window substring.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        if t == '': return ''
+        countT, window = {}, {}
+
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resL = [-1, -1], float('inf')
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                if resL > r - l + 1:
+                    resL = r - l + 1
+                    res = [l, r]
+                  
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l: r+1] if resL != float('inf') else ''
