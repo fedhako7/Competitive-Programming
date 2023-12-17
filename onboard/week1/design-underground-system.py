65af70b684b299de4d93558ab48632f8b6c8481d
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2023-12-17 23:55:45 +0300

    Added solution for Design Underground System

---
+++ onboard/week1/design-underground-system.py
@@ -0,0 +1,27 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        self.duration = 0
+        self.by_id = {}
+        self.by_s = {}
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.by_id[id] = [stationName, t]
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        [ss, t1] = self.by_id[id]
+        self.by_id[id] = [ss, t, stationName, t]
+
+        group_by_e = self.by_s.get(ss)
+        if group_by_e is None:
+            group_by_e = {}
+            self.by_s[ss] = group_by_e
+        group_by_time = group_by_e.get(stationName)
+        if group_by_time is None:
+            group_by_time = []
+            group_by_e[stationName] = group_by_time
+        group_by_time.append(t - t1)
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        acc = self.by_s[startStation][endStation]
+        return sum(acc) / len(acc)
