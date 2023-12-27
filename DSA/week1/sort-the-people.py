8c71fd70f7bb40fa6693d4abff9285273474544c
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2023-12-27 09:40:36 +0300

    Added solution for Sort the People

--- DSA/week1/sort-the-people.py
+++ DSA/week1/sort-the-people.py
@@ -1,9 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(names)):
-            for j in range(1, len(names)):
-                if heights[j - 1] < heights[j]:
-                    heights[j - 1], heights[j] = heights[j], heights[j - 1]
-                    names[j - 1], names[j] = names[j], names[j - 1]
+        for idx in range(1, len(names)):
+            key = heights[idx]
+            curr_n = names[idx]
+            idx2 = idx - 1
+            while idx2 > -1 and key > heights[idx2]:
+                heights[idx2 + 1] = heights[idx2]
+                names[idx2 + 1] = names[idx2]                
+                idx2 -= 1
+
+            heights[idx2 + 1] = key
+            names[idx2 + 1] = curr_n
+
 
         return names
