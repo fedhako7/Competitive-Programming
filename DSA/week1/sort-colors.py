c0fa2ce60f918ab6233da3188c4b4684841080a4
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2024-01-10 07:44:34 +0300

    Added solution for Sort Colors

--- DSA/week1/sort-colors.py
+++ DSA/week1/sort-colors.py
@@ -1,21 +1,18 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        def triPartition(nums, target):
-            i, j, n = 0, 0, len(nums) - 1
+        def compar(num1, num2):
+            if num1 < num2:
+                return -1
 
-            while j <= n:
-                if nums[j] < target:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    i += 1
-                    j += 1
-                elif nums[j] > target:
-                    nums[j], nums[n] = nums[n], nums[j]
-                    n -= 1
-                else:
-                    j += 1
+            elif num2 < num1:
+                return 1
+            else:
+                return 0
 
-        triPartition(nums, 1)
+        
+        nums.sort(key = cmp_to_key(compar))
+
+        return nums
