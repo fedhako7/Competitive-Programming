d6e142dbcec4840f8174f242c5f3e7d67ae387bd
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2024-01-11 19:50:49 +0300

    Add solution for Remove Element

---
+++ DSA/Sliding Window/leetcode/remove-element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left, right = 0, 0
+
+        while right < len(nums):
+            while right < len(nums) and nums[right] == val:
+                right += 1
+
+            if left < len(nums) and right < len(nums):
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right += 1
+
+
+        return left
+
+        
