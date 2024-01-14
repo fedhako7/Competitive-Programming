e7eba285d2851fd1708cfb33e16a41c202ba8fbb
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2024-01-14 19:05:44 +0300

    Add solution for Longest Repeating Character Replacement

---
+++ DSA/Sliding Window/leetcode/longest-repeating-character-replacement.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+class Solution:
+    def characterReplacement(self, s, k):
+        maxFreq = 0
+        result=0
+        counter = Counter()
+        
+        slow = 0
+        fast = 0
+        N = len(s)
+        while fast<N:
+            counter[s[fast]]+=1
+            maxFreq = max(maxFreq, counter[s[fast]])
+            
+            if (fast-slow+1)-maxFreq > k:
+                counter[s[slow]]-=1
+                slow+=1
+            result = max(result, (fast-slow+1))    
+            fast+=1
+            
+        return result
