5b04876f6d37ad74fb98a4fbc7b1746cd969a0c7
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2024-02-19 16:50:48 +0300

    Add solution for Score of Parentheses

---
+++ DSA/Sliding Window/leetcode/score-of-parentheses.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def scoreOfParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        
+        """
+        op = []
+        for a in s:
+            if a == '(':
+                op.append(a)
+            elif a == ')':
+                last_item = op.pop()                         
+                if last_item == '(':
+                    op.append(1)
+                else:
+                    op[-1] = last_item * 2
+
+            if len(op) >= 2:
+                if isinstance(op[-1], int) and isinstance(op[-2], int):
+                    op.append(op.pop() + op.pop())
+
+        return op[0]
+
+
+                
+        
