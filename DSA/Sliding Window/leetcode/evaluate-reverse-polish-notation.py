e605cad3cfa8dc57a5880e4b2188eefb1646801b
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2024-02-19 16:52:34 +0300

    Add solution for Evaluate Reverse Polish Notation

---
+++ DSA/Sliding Window/leetcode/evaluate-reverse-polish-notation.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+
+        nums = []
+        for elem in tokens:
+            if elem not in "+-*/":
+                nums.append(elem)
+            elif elem == '+':
+                op1 = int(nums.pop())
+                op2 = int(nums.pop())
+                nums.append(op1 + op2)
+
+            elif elem == '-':
+                op1 = int(nums.pop())
+                op2 = int(nums.pop())
+                nums.append(op2 - op1)
+
+            elif elem == '*':
+                op1 = int(nums.pop())
+                op2 = int(nums.pop())
+                nums.append(op1 * op2)
+            elif elem == '/':
+                op1 = int(nums.pop())
+                op2 = int(nums.pop())
+                nums.append(int(op2/float(op1)))
+
+        return int(nums[0])
+            
